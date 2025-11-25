7ffe92030064b0ae42326066a5cab106d26b3bf5
liekensced <liekensced@gmail.comg>
2025-11-25 09:56:32 +0100

    cleaning

--- DroneRadarBackend/backend/app/main.py
+++ DroneRadarBackend/backend/app/main.py
@@ -15,26 +15,6 @@ app = FastAPI(title='Planes backend')
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
-# Add CORS middleware for frontend access
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost",
-        "http://localhost:3000",
-        "http://localhost:5000",
-        "http://localhost:8080",
-        "http://localhost:8000",
-        "http://127.0.0.1",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:5000",
-        "http://127.0.0.1:8080",
-        "http://127.0.0.1:8000",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 # Include routers
 app.include_router(planes.router)
 app.include_router(images.router)

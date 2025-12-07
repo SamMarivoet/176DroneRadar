e42584b51904b10d7b21e15504becd87f911a607
VBOR732 <vnulmans@gmail.com>
2025-12-07 15:18:36 +0100

    Fix icon URL for OGN source to use lowercase

--- Map_GUI/frontend/app.js
+++ Map_GUI/frontend/app.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", () => {
           if (p.source == 'ogn') {
             countryOrType = "Type: ";
             if (!showOgn) return;
-            iconUrl = 'icons/GliderOK.png';
+            iconUrl = 'icons/glider.png';
           }
 
           const icon = L.icon({

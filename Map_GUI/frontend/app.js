2c9e8c539e6e9f5681e24f9fa9ab4e1ff7c0127b
VBOR732 <vnulmans@gmail.com>
2025-12-07 14:57:09 +0100

    Change icon URL for OGN source to GliderC

--- Map_GUI/frontend/app.js
+++ Map_GUI/frontend/app.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", () => {
           if (p.source == 'ogn') {
             countryOrType = "Type: ";
             if (!showOgn) return;
-            iconUrl = 'icons/gliderrr.png';
+            iconUrl = 'icons/GliderC.png';
           }
 
           const icon = L.icon({
